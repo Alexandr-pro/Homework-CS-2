aa8fc7d83982ec7435cfeb5f2ca84d9f733a1c2c
«Alexander» <semeon.gromow@yandex.ru>
2022-11-27 00:30:22 +0300

    Homework_2 task №13

---
+++ Exp002/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int Pound_3(int n){
+    while (n > 999){
+        n /= 10;
+    }
+    return n % 10;
+}
+bool BadOption(int n){    
+    if (n<100){
+        Console.WriteLine ($"Такого числа нет");
+        return false;
+    }
+    return true;
+}
+
+Console.WriteLine("Введите трехзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (BadOption(n)){
+    Console.WriteLine(Pound_3(n));
+}
